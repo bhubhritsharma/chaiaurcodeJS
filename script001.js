c46724d8c7e122c675993e2ad2fa64db5afffb34
Bhubhrit Sharma <100941365+bhubhritsharma@users.noreply.github.com>
2024-10-05 01:36:58 +0530

    updated script_001.js

---
+++ script001.js
@@ -0,0 +1,16 @@
+console.log("Bhubhrit Sharma");
+const accountId = 123545;
+let accountEmail = "abc@gmail.com";
+var accountPwd = "123456789";
+accountCity = "Chandigarh;";
+let accountState;
+
+// accountId = 23456; //not allowed
+// console.log("accountId: ", accountId);
+
+accountEmail = "xyz@hotmail.com";
+accountPwd = "999999900";
+accountCity = "Delhi";
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState]);
+
