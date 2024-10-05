1b6973934b0d2b7ccc7ceccefd6daac978fdea37
Bhubhrit Sharma <100941365+bhubhritsharma@users.noreply.github.com>
2024-10-05 08:40:36 +0530

    script_002.js added

---
+++ script_002.js
@@ -0,0 +1,21 @@
+"use strict"; //This treats all JS code as per newer version
+
+// alert("Hello !!")
+
+let name = "Bhubhrit";
+let age = 18;
+let isLoggedIn = true;
+
+// number
+// bigInt
+// string
+// boolean
+// null - standalone value
+// undefined - value is not asigned
+// symbol - to find uniqueness
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
+
